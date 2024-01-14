df3ca3a003cdd510036072e97b99769fde264f4f
NEPERSONAJ <141691041+NEPERSONAJ@users.noreply.github.com>
2024-01-14 23:21:28 +0300

    _app.page.js

--- src/pages/_app.page.js
+++ src/pages/_app.page.js
@@ -20,7 +20,7 @@ export const AppContext = createContext({});
 const repoPrompt = `
 __  __  __
 \u005C \u005C \u005C \u005C \u005C\u2215\n \u005C \u005C\u2215\u005C \u005C\n  \u005C\u2215  \u005C\u2215
-\n\nTaking a peek huh? Check out the source code: https://github.com/HamishMW/portfolio
+\n\n
 `;
 
 const App = ({ Component, pageProps }) => {
@@ -55,7 +55,7 @@ const App = ({ Component, pageProps }) => {
               className={styles.skip}
               href="#MainContent"
             >
-              Skip to main content
+              Перейти к основному содержанию
             </VisuallyHidden>
             <Navbar />
             <main className={styles.app} tabIndex={-1} id="MainContent">
