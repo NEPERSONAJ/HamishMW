b295d6fa95b4e14c82dad2f78de673f5391608ed
NEPERSONAJ <141691041+NEPERSONAJ@users.noreply.github.com>
2024-01-15 21:58:56 +0300

    index.js

--- functions/index.js
+++ functions/index.js
@@ -1,87 +1,73 @@
 const serverless = require('serverless-http');
+const nodemailer = require('nodemailer');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const createDOMPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
-const aws = require('@aws-sdk/client-ses');
 
 const app = express();
 const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
+const { smtpHost, smtpUser, smtpPass } = process.env;
 
-const ses = new aws.SES({
-  region: 'us-east-1',
+const mailTransport = nodemailer.createTransport({
+  host: 'smtp.nepersonaj.ru',
+  port: 587,
+  secure: false, // Используйте true, если ваш SMTP-сервер требует безопасного соединения
+  auth: {
+    user: 'support@nepersonaj.ru',
+    pass: 'SsDA@EDS2sder#s',
+  },
 });
 
-const ORIGINS = ['https://nepersonaj.ru', 'https://nepersonaj.ru'];
+const ORIGIN = 'https://nepersonaj.ru';
 const MAX_EMAIL_LENGTH = 512;
 const MAX_MESSAGE_LENGTH = 4096;
-const EMAIL = 'support@nepersonaj.ru';
-const FROM_EMAIL = 'mailbot@nepersonaj.ru';
-const EMAIL_PATTERN = /(.+)@(.+){2,}\.(.+){2,}/;
 
 app.use(helmet());
 app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!ORIGINS.includes(origin)) {
-        return callback(
-          new Error(`Не разрешено CORS. Происхождение должно быть: ${ORIGINS.join(' or ')}`)
-        );
-      }
-
-      return callback(null, true);
-    },
-  })
-);
-app.options('*', cors());
+app.use(cors({ origin: ORIGIN }));
 
 app.post('/message', async (req, res) => {
   try {
     const email = DOMPurify.sanitize(req.body.email);
     const message = DOMPurify.sanitize(req.body.message);
 
+    // Reject unsupported origins
+    if (req.headers.origin !== ORIGIN) {
+      throw new Error(`Unsupported origin: ${req.headers.origin}`);
+    }
+
     // Validate email request
-    if (!email || !EMAIL_PATTERN.test(email)) {
-      return res.status(400).json({ error: 'Пожалуйста, введите действительный адрес электронной почты' });
-    }
-
-    if (!message) {
-      return res.status(400).json({ error: 'Пожалуйста, введите сообщение' });
-    }
-
-    if (email.length > MAX_EMAIL_LENGTH) {
+    if (!email || !/(.+)@(.+){2,}\.(.+){2,}/.test(email)) {
+      return res.status(400).json({ error: 'Please enter a valid email address' });
+    } else if (!message) {
+      return res.status(400).json({ error: 'Please enter a message' });
+    } else if (email.length > MAX_EMAIL_LENGTH) {
       return res.status(400).json({
-        error: `Пожалуйста, в email используйте меньше чем ${MAX_EMAIL_LENGTH} символов`,
+        error: `Please enter an email fewer than ${MAX_EMAIL_LENGTH} characters`,
+      });
+    } else if (message.length > MAX_MESSAGE_LENGTH) {
+      return res.status(400).json({
+        error: `Please enter a message fewer than ${MAX_MESSAGE_LENGTH} characters`,
       });
     }
 
-    if (message.length > MAX_MESSAGE_LENGTH) {
-      return res.status(400).json({
-        error: `Пожалуйста, используйте меньше чем ${MAX_MESSAGE_LENGTH} символов`,
-      });
-    }
+    // Send email
+    const mailOptions = {
+      from: 'NEPERSONAJ <support@nepersonaj.ru>',
+      to: 'support@nepersonaj.ru',
+      subject: `New message from ${email}`,
+      text: `From: ${email}\n\n${message}`,
+    };
 
-    // Send email using AWS SES
-    await ses.sendEmail({
-      Source: `Portfolio <${FROM_EMAIL}>`,
-      Destination: {
-        ToAddresses: [EMAIL],
-      },
-      Message: {
-        Subject: { Data: `Новое сообщение от ${email}` },
-        Body: {
-          Text: { Data: `От: ${email}\n\n${message}` },
-        },
-      },
-    });
+    await mailTransport.sendMail(mailOptions);
 
-    return res.status(200).json({ message: 'Сообщение успешно отправлено' });
+    return res.status(200).json({ message: 'Message sent successfully' });
   } catch (error) {
     console.error('Rejected', error);
-    return res.status(500).json({ error: 'Сообщение отклонено' });
+    return res.status(500).json({ error: 'Message rejected' });
   }
 });
 
