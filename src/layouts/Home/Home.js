d816054365bce747f2ee04dc659e5d0e45124e8b
NEPERSONAJ <141691041+NEPERSONAJ@users.noreply.github.com>
2024-01-19 18:23:03 +0300

    Home.js

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -18,7 +18,7 @@ import { ProjectSummary } from 'layouts/Home/ProjectSummary';
 import { useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 
-const disciplines = ['Сайты', 'Программы', 'Игры', 'Сео', 'Продвижение'];
+const disciplines = ['Сайты', 'Приложения', 'Игры', 'SEO', 'Маркетинг'];
 
 export const Home = () => {
   const [visibleSections, setVisibleSections] = useState([]);
