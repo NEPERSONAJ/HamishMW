c2f907083d2022f4a385775e59c61d2c97e4387b
NEPERSONAJ <141691041+NEPERSONAJ@users.noreply.github.com>
2024-01-26 04:36:37 +0300

    Slice.js

--- src/pages/projects/slice/Slice.js
+++ src/pages/projects/slice/Slice.js
@@ -59,7 +59,7 @@ export const Slice = () => {
         <ProjectHeader
           title={title}
           description={description}
-          url="https://www.best.edu.au/s/q2yjjvl7?data=8%404!9%4020303!10%40-15087&version=1"
+          url="https://t.me/smmbro24"
           roles={roles}
         />
         <ProjectSection padding="top">
@@ -75,12 +75,29 @@ export const Slice = () => {
         <ProjectSection>
           <ProjectSectionColumns centered className={styles.columns}>
             <div className={styles.imagesText}>
-              <ProjectSectionHeading>Начало</ProjectSectionHeading>
+              <ProjectSectionHeading>История</ProjectSectionHeading>
               <ProjectSectionText>
-              В мире гейминга наступила эпоха перемен, и я, как опытный программист и фрилансер, стал свидетелем твоего вдохновляющего запроса. Один клиент поделился со мной своей историей усталости от рутинной работы по созданию стартовых аккаунтов для увлекательной гачи игры. Ежедневное ручное трудоемкое создание аккаунтов стало преградой на пути к незабываемым игровым приключениям.
+             Мой клиент был предпринимателем, зарабатывающим на продаже стартовых готовых аккаунтов в гача-играх. Его подход был интересен: он создавал множество аккаунтов, каждый день получая бесплатный призыв с шансом выпадения крутых легендарных персонажей. Успех и прибыль зависели от крутости персонажа и их количества.
               </ProjectSectionText>
+              <ProjectSectionHeading>Вызовы и Решения</ProjectSectionHeading>
               <ProjectSectionText>
-              Встреча с этим клиентом стала точкой отсчета для создания нечто уникального и инновационного. Его запрос на разработку программы, способной автоматизировать процесс создания аккаунтов, обучения персонажей и ежедневного сбора наград, взволновал мои творческие потребности и стал стимулом для создания чего-то по-настоящему удивительного.
+              Проблема: Рутина и Затраты Времени
+Основная проблема заключалась в том, что на каждом аккаунте ему приходилось проходить обучение для активации стадии забора ежедневных наград. Это было трудоемким и отнимало слишком много времени. К тому же, ежедневное управление множеством аккаунтов становилось непосильной задачей.
+
+Решение: Программа Обучения
+Сначала я создал программу, которая проходила обучение на аккаунтах. Теперь клиент мог автоматизировать этот процесс и сэкономить массу времени.
+
+Проблема: Ручной Забор Наград
+Ежедневный забор наград также стал большой рутиной. Клиент тратил часы, заходя в каждый аккаунт и делая многочисленные манипуляции для получения ежедневных бонусов.
+
+Решение: Программа для Сбора Наград
+Создана вторая программа, которая автоматизировала процесс забора ежедневных бонусов на всех аккаунтах и скриншотинг каждого нового героя с сохранением в определенную папку с названием аккаунта. Теперь клиент мог наслаждаться бесперебойным потоком ресурсов без лишних усилий.
+
+Проблема: Избыточные Скрины Героев
+При продаже аккаунтов клиенту не нужны были скрины простых героев, что усложняло процесс. Это стало дополнительной рутиной, которая также требовала времени.
+
+Решение: Программа для Скриншотов
+Создана третья программа, которая удаляла лишние скрины героев, которые не влияли на ценность аккаунта для продажи.
               </ProjectSectionText>
             </div>
             <div className={styles.sidebarImages}>
@@ -88,14 +105,14 @@ export const Slice = () => {
                 className={styles.sidebarImage}
                 srcSet={[sliceSidebarLayers, sliceSidebarLayersLarge]}
                 placeholder={sliceSidebarLayersPlaceholder}
-                alt="The layers sidebar design, now with user profiles."
+                alt="Начало"
                 sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
               />
               <Image
                 className={styles.sidebarImage}
                 srcSet={[sliceSidebarAnnotations, sliceSidebarAnnotationsLarge]}
                 placeholder={sliceSidebarAnnotationsPlaceholder}
-                alt="Multiple user annotations on a shared layer."
+                alt="Этап"
                 sizes={`(max-width: ${media.mobile}px) 200px, 343px`}
               />
             </div>
@@ -104,15 +121,15 @@ export const Slice = () => {
         <ProjectSection light>
           <ProjectSectionContent>
             <ProjectTextRow>
-              <ProjectSectionHeading>Этап</ProjectSectionHeading>
+              <ProjectSectionHeading>Создание Специфического Веб-Сайта</ProjectSectionHeading>
               <ProjectSectionText>
-              Создание этой программы стало настоящим вызовом, который потребовал сочетания выдающихся навыков программирования и глубокого понимания игровой механики. Я стремился к тому, чтобы результат был не просто эффективным, но и элегантным. Программа должна была быть как невидимым волшебством, которое освобождает геймера от тяжкой рутины и позволяет ему полностью наслаждаться игрой.
+              Для удобства управления и монетизации создан специфический веб-сайт. Теперь клиент мог загружать списки аккаунтов с героями, ресурсами и ценами для удобного просмотра и управления.
               </ProjectSectionText>
             </ProjectTextRow>
             <Image
               srcSet={[sliceSlides, sliceSlidesLarge]}
               placeholder={sliceSlidesPlaceholder}
-              alt="The new My Slides tab in slice, showing annotated and favorited slides."
+              alt="Специфический Веб-Сайт"
               sizes={`(max-width: ${media.mobile}px) 500px, (max-width: ${media.tablet}px) 800px, 1000px`}
             />
           </ProjectSectionContent>
@@ -133,15 +150,15 @@ export const Slice = () => {
                 <Image
                   srcSet={[sliceAnnotation, sliceAnnotationLarge]}
                   placeholder={sliceAnnotationPlaceholder}
-                  alt="An annotation preview popover with statistics for shape perimeter and area."
+                  alt="Отношения"
                   sizes={`(max-width: ${media.mobile}px) 584px, (max-width: ${media.tablet}px) 747px, 556px`}
                 />
               </div>
             </div>
             <div className={styles.gridText}>
-              <ProjectSectionHeading>Подход</ProjectSectionHeading>
+              <ProjectSectionHeading>Результаты и Успех</ProjectSectionHeading>
               <ProjectSectionText>
-              Процесс разработки включал в себя тщательный анализ игровых механик, создание инновационных алгоритмов и максимальную оптимизацию для обеспечения безупречной производительности. Результатом стал интеллектуальный инструмент, способный не только создавать аккаунты и проходить обучение, но и адаптироваться к изменениям в игровом процессе, обеспечивая долгосрочную и бесперебойную работу.
+              Благодаря этим трем программам и веб-сайту, клиент стал владельцем полностью автоматизированного бизнеса в мире гача-игр. Его ежемесячный заработок превысил 100 тысяч рублей, а он освободил массу времени для более интересных и прибыльных занятий.
               </ProjectSectionText>
             </div>
           </ProjectSectionContent>
@@ -149,15 +166,15 @@ export const Slice = () => {
         <ProjectSection>
           <ProjectSectionContent>
             <ProjectTextRow>
-              <ProjectSectionHeading>Отношения</ProjectSectionHeading>
+              <ProjectSectionHeading>Заключение</ProjectSectionHeading>
               <ProjectSectionText>
-              Сотрудничество с этим клиентом превратилось в настоящее приключение, где каждая линия кода стала шагом в направлении к созданию чего-то великолепного. Теперь, благодаря разработанной программе, он может зарабатывать на этой игре без лишних забот и радоваться ежедневным крутым героям на разных аккаунтах.
+              Эта история является примером того, как я, понимая потребности клиента и глубоко анализируя механику игры, может трансформировать рутину в автоматизированный успех. Если у вас есть потребность в подобных программных решениях, не стесняйтесь связаться со мной. Мы вместе сможем создать что-то уникальное и эффективное для вашего бизнеса.
               </ProjectSectionText>
             </ProjectTextRow>
             <Image
               src={sliceIrl}
               placeholder={sliceIrlPlaceholder}
-              alt="Students at the University of New South Wales using the new collaborative annotation features"
+              alt="Заключение"
             />
           </ProjectSectionContent>
         </ProjectSection>
