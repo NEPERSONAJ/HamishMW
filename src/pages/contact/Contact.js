ae277fe05404de01188267f2d1f4bb7bb1a69f92
NEPERSONAJ <141691041+NEPERSONAJ@users.noreply.github.com>
2025-02-12 16:10:34 +0300

    Update Contact.js

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,6 +1,7 @@
 import { Button } from 'components/Button';
 import { DecoderText } from 'components/DecoderText';
 import { Divider } from 'components/Divider';
+import { Footer } from 'components/Footer';
 import { Heading } from 'components/Heading';
 import { Icon } from 'components/Icon';
 import { Input } from 'components/Input';
